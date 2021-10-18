0bf556a62baac52ef3dbfff7e14ee83dca5d2d15
Comeom Rice <cch9399@stern.nyu.edu>
2021-10-18 17:28:01 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Wayne on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
